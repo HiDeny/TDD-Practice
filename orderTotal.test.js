f2c5ae09b97241a35ada4c41c5cb275c189d9c58
HiDeny <114344945+HiDeny@users.noreply.github.com>
2023-10-19 11:49:44 +0200

    few more jest tests

--- orderTotal.test.js
+++ orderTotal.test.js
@@ -4,6 +4,12 @@ it('works', () => {
   expect(1).toBe(1);
 });
 
+it('object assignment', () => {
+  const data = { one: 1 };
+  data['two'] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
 it('Quantity', () => {
   expect(
     orderTotal({
@@ -12,32 +18,32 @@ it('Quantity', () => {
   ).toBe(6);
 });
 
-if (
-  orderTotal({
-    items: [
-      { name: 'Dragon food', price: 8 },
-      { name: 'Dragon cage (small)', price: 800 },
-    ],
-  }) !== 808
-) {
-  throw new Error('Check fail: Happy path (Example 1)');
-}
+it('Happy path 1', () => {
+  expect(
+    orderTotal({
+      items: [
+        { name: 'Dragon food', price: 8 },
+        { name: 'Dragon cage (small)', price: 800 },
+      ],
+    })
+  ).toBe(808);
+});
 
-if (
-  orderTotal({
-    items: [{ name: 'Dragon candy', price: 3 }],
-  }) !== 3
-) {
-  throw new Error('Check fail: No quantity specified');
-}
+it('Happy path 2', () => {
+  expect(
+    orderTotal({
+      items: [
+        { name: 'Dragon collar', price: 20, quantity: 1 },
+        { name: 'Dragon chew toy', price: 40, quantity: 1 },
+      ],
+    })
+  ).toBe(60);
+});
 
-if (
-  orderTotal({
-    items: [
-      { name: 'Dragon collar', price: 20, quantity: 1 },
-      { name: 'Dragon chew toy', price: 40, quantity: 1 },
-    ],
-  }) !== 60
-) {
-  throw new Error('Check fail: Happy path (Example 2)');
-}
+it('No quality specified', () => {
+  expect(
+    orderTotal({
+      items: [{ name: 'Dragon candy', price: 3 }],
+    })
+  ).toBe(3);
+});
