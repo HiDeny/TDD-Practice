ea568e55ad7ec8de9865a6f1d9bb2c754ec82643
HiDeny <114344945+HiDeny@users.noreply.github.com>
2023-10-19 15:54:38 +0200

    all solved

--- odinPractice.js
+++ odinPractice.js
@@ -8,4 +8,57 @@ export const capitalize = (str) => {
 // Takes a string and returns it reversed.
 export const reverseString = (str) => str.split('').reverse().join('');
 
-console.log(reverseString('Test'));
+// Contains functions for the basic operations: add, subtract, divide, and multiply.
+// Each of these functions should take two numbers and return the correct calculation.
+export const calculator = {
+  add: (num1, num2) => num1 + num2,
+  subtract: (num1, num2) => num1 - num2,
+  divide: (num1, num2) => num1 / num2,
+  multiply: (num1, num2) => num1 * num2,
+};
+
+// takes a string and a shift factor and returns it with each character “shifted”.
+export const caesarCipherDecoder = (str, key) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabetUpperCase = alphabet.toUpperCase();
+  let result = '';
+
+  const stringSplit = str.split('');
+
+  stringSplit.forEach((letter) => {
+    let index = alphabet.indexOf(letter);
+    let indexUpper = alphabetUpperCase.indexOf(letter);
+
+    if (index == -1 && indexUpper == -1) result += letter;
+
+    if (index > -1 && indexUpper == -1) {
+      let newIndex = (index + key) % 26;
+      result += alphabet[newIndex];
+    }
+    if (index == -1 && indexUpper > -1) {
+      let newIndex = (indexUpper + key) % 26;
+      result += alphabetUpperCase[newIndex];
+    }
+  });
+
+  return result;
+};
+
+// takes an array of numbers and returns an object with the following properties:
+//* average, min, max, and length.
+
+export const analyzeArray = (arr) => {
+  const sum = arr.reduce((acc, current) => acc + current, 0);
+  console.log(sum);
+
+  return {
+    average: Math.floor(sum / arr.length),
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
+};
+
+console.log(analyzeArray([1, 8, 3, 4, 2, 6]));
+console.log(analyzeArray([10, 8, 3, 4, 2, 6]));
+
--- odinPractice.test.js
+++ odinPractice.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverseString } from './odinPractice';
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipherDecoder,
+  analyzeArray,
+} from './odinPractice';
 
 // Takes a string and returns it with the first character capitalized.
 it('Capitalize1', () => {
@@ -25,3 +31,86 @@ it('Reverse String', () => {
 it('Reverse String', () => {
   expect(reverseString('Stoic')).toBe('ciotS');
 });
+
+// Contains functions for the basic operations: add, subtract, divide, and multiply.
+// Each of these functions should take two numbers and return the correct calculation.
+
+it('Calculator: contain operations', () => {
+  expect(calculator).toHaveProperty('add');
+  expect(calculator).toHaveProperty('subtract');
+  expect(calculator).toHaveProperty('divide');
+  expect(calculator).toHaveProperty('multiply');
+});
+
+it('Calculator: add', () => {
+  expect(calculator.add(10, 10)).toBe(20);
+});
+
+it('Calculator: subtract', () => {
+  expect(calculator.subtract(10, 10)).toBe(0);
+});
+
+it('Calculator: divide', () => {
+  expect(calculator.divide(10, 10)).toBe(1);
+});
+
+it('Calculator: multiply', () => {
+  expect(calculator.multiply(10, 10)).toBe(100);
+});
+
+it('Calculator: multiply', () => {
+  expect(calculator.multiply(10, 100)).toBe(1000);
+});
+
+// takes a string and a shift factor and returns it with each character “shifted”.
+it('Cesar Cipher', () => {
+  expect(caesarCipherDecoder('z', 1)).toBe('a');
+});
+
+it('Cesar Cipher', () => {
+  expect(caesarCipherDecoder('A', 1)).toBe('B');
+});
+
+it('Cesar Cipher', () => {
+  expect(caesarCipherDecoder('.,?!', 1)).toBe('.,?!');
+});
+
+it('Cesar Cipher', () => {
+  expect(caesarCipherDecoder('defend the east wall of the castle', 1)).toBe(
+    'efgfoe uif fbtu xbmm pg uif dbtumf'
+  );
+});
+
+it('Cesar Cipher', () => {
+  expect(caesarCipherDecoder('hello', 3)).toBe('khoor');
+});
+
+it('Cesar Cipher', () => {
+  expect(caesarCipherDecoder('openai', 5)).toBe('tujsfn');
+});
+
+it('Cesar Cipher', () => {
+  expect(caesarCipherDecoder('example', 7)).toBe('lehtwsl');
+});
+
+// takes an array of numbers and returns an object with the following properties: average, min, max, and length.
+it('Analyze Array', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
+it('Analyze Array', () => {
+  const object = analyzeArray([1, 10, 3, 4, 2, 6]);
+  expect(object.max).toBe(10);
+});
+
+it('Analyze Array', () => {
+  const object = analyzeArray([1, 10, 3, 4, 2, 6]);
+  expect(object.min).toBe(1);
+});
+
+
